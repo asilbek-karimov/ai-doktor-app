e882323eaa794b0c72f8d5606354ba07d5e5c41a
Asilbek Karimov <asilbekkarimov004@gmail.com>
2025-07-29 12:07:12 +0500

    Tuzatish: name noto‘g‘ri yozilgan edi

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ﻿from flask import Flask, request, render_template_string
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -18,5 +18,5 @@ def index():
         <p>{{ javob }}</p>
     """, javob=javob)
 
-if name == "main":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
